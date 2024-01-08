1c25bb668801a1cc9d239baac786a6ae2d610982
seiyaworld <155869329+seiyaworld@users.noreply.github.com>
2024-01-08 17:23:20 +0900

    Revert "completed post function"

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,3 @@
 Rails.application.routes.draw do
-
-  devise_for :users, controllers: {
-    registrations: "public/registrations",
-    sessions: "public/sessions"
-  }
-  
-  scope module: :public do
-    root to: 'homes#top'
-    get 'about' => 'homes#about'
-    resources :users
-    resources :posts
-  end
-  
-  
-  
-  
-  
-  
-  
-  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
