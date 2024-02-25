42511ea7329e42c08b62ea35fb09e4f2843f0312
EC2 Default User <ec2-user@ip-172-31-25-61.ap-northeast-1.compute.internal>
2024-02-25 08:05:27 +0000

    First

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -2,7 +2,9 @@ class Public::PostsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @posts = Post.all
+    to = Time.current.at_end_of_day
+	  from = (to-6.day).at_beginning_of_day
+	  @posts = Post.includes(:favorites).sort_by {|post| -post.favorites.where(created_at: from...to).count}
     @new_post = Post.new
   end
 
